c927e0b910a18feaf1d8e2b8584b2ade93c2dd69
Capi Etheriel <barraponto@gmail.com>
2015-11-20 16:48:48 -0200

    life is hard for christina

--- tbay.py
+++ tbay.py
@@ -39,3 +39,32 @@ class Bid(Base):
     bidder = relationship('User', backref="bids")
 
 Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    from database import session
+    from sqlalchemy import desc
+
+    beyonce = User(username='bknowles', password='123456')
+    christina = User(username='caguillera', password='qweasd')
+    britney = User(username='bspears', password='secret')
+
+    microphone = Item(name="microphone",
+                      description="Mika's gold microphone.",
+                      owner=britney)
+
+    bids = [
+         Bid(price=100.0, bidder=christina, item=microphone),
+         Bid(price=150.0, bidder=beyonce, item=microphone),
+         Bid(price=200.0, bidder=christina, item=microphone),
+         Bid(price=201.0, bidder=beyonce, item=microphone),
+    ]
+
+    session.add_all([beyonce, christina, britney, microphone] + bids)
+    session.commit()
+
+    highest_bid = (session.query(Bid)
+                   .filter(Bid.item==microphone)
+                   .order_by(desc(Bid.price))
+                   .first())
+
+    print(highest_bid.bidder.username)
